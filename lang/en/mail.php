7ef1ea1abf588ba205d07ed3acc5fa9c35db19a6
lastdino <sou.godzilla@gmail.com>
2025-07-02 19:51:53 +0900

    英語・日本語の翻訳ファイルを更新:
    - 承認フロー関連の新しい翻訳キーを追加 (`status`, `request_from`, `check`, `labels.status`)
    - 一部キーの表記を修正 (`thank_you` → `thank`)

--- lang/en/mail.php
+++ lang/en/mail.php
@@ -1,7 +1,18 @@
 <?php
 
 return [
+    'request_from'=>'There is a :flow request from Mr./Ms. :name .',
+    'check' => 'Please check it.',
     'confirm' => 'confirm',
     'ignore' => 'If this message does not resonate with you, please ignore this email.',
-    'thank_you'=>'thank you'
+    'thank'=>'thank you',
+    'status' => [
+        'pending'=>'Pending',
+        'approved' => 'Approved',
+        'rejected' => 'Rejected',
+        'cancelled' => 'Cancelled',
+    ],
+    'labels' => [
+        'status' => 'Status:',
+    ]
 ];
--- lang/ja/mail.php
+++ lang/ja/mail.php
@@ -1,7 +1,18 @@
 <?php
 
 return [
+    'request_from'=>':nameさんから:flow申請がありました。',
+    'check' => '確認お願いします。',
     'confirm' => '確認する。',
     'ignore' => 'このメッセージに心当たりがない場合は、このメールを無視してください。',
-    'thank_you'=>'ありがとうございます。'
+    'thank'=>'ありがとうございます。',
+    'status' => [
+        'pending'=>'保留中',
+        'approved' => '承認済み',
+        'rejected' => '却下',
+        'cancelled' => 'キャンセル',
+    ],
+    'labels' => [
+        'status' => '状態:',
+    ],
 ];
